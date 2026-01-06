7c2b66e7509d0fbc571256cd1e9ea584d9ba127a
Tim de Klijn <timdeklijn@gmail.com>
2026-01-06 14:53:37 +0100

    fix: displaylink packages again

--- files/scripts/displaylink.sh
+++ files/scripts/displaylink.sh
@@ -3,22 +3,27 @@
 set -oue pipefail
 
 dnf -y copr enable crashdummy/Displaylink
-# dnf -y install eza
+dnf update 
+dnf -y install \
+  displaylink \
+  dkms \
+  kernel-devel \
+  kernel-headers
 
-# - dkms
-# - kernel-devel
-# - kernel-headers
-# - displaylink
-# - libevdi
-# depending on what's provided for your Fedora/kernel:
-# - kmod-evdi
+# # - dkms
+# # - kernel-devel
+# # - kernel-headers
+# # - displaylink
+# # - libevdi
+# # depending on what's provided for your Fedora/kernel:
+# # - kmod-evdi
 
-dnf5 repolist
-dnf5 repoquery --available '*evdi*' '*displaylink*'
+# dnf repolist
+# dnf repoquery --available '*evdi*' '*displaylink*'
 
-# Find the kernel version(s) installed in the image (not the host kernel)
-KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core | tail -n1)"
-# Build kmods
-akmods --force --kernels "$KVER"
-# Generate module dependency info 
-depmod -a "$KVER"
+# # Find the kernel version(s) installed in the image (not the host kernel)
+# KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core | tail -n1)"
+# # Build kmods
+# akmods --force --kernels "$KVER"
+# # Generate module dependency info 
+# depmod -a "$KVER"
