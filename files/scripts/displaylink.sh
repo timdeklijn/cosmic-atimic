c4d057b4bcd25fbfa9f7ec1c2678bbd127737701
Tim de Klijn <timdeklijn@gmail.com>
2026-01-06 14:20:13 +0100

    fix: displaylink packages

--- files/scripts/displaylink.sh
+++ files/scripts/displaylink.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
 set -oue pipefail
-
-dnf5 install -y \
-    akmod-evdi \
-    displaylink \
-    dkms \
-    kernel-devel \
-    kernel-headers
 
 # Find the kernel version(s) installed in the image (not the host kernel)
 KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core | tail -n1)"
