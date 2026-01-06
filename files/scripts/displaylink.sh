39568e9246169b02b0c4d427d0d76ce70f1e7454
Tim de Klijn <timdeklijn@gmail.com>
2026-01-06 14:43:55 +0100

    debug: find displaylink repos again again

--- files/scripts/displaylink.sh
+++ files/scripts/displaylink.sh
@@ -2,6 +2,20 @@
 
 set -oue pipefail
 
+dnf -y copr enable crashdummy/Displaylink
+# dnf -y install eza
+
+# - dkms
+# - kernel-devel
+# - kernel-headers
+# - displaylink
+# - libevdi
+# depending on what's provided for your Fedora/kernel:
+# - kmod-evdi
+
+dnf5 repolist
+dnf5 repoquery --available '*evdi*' '*displaylink*'
+
 # Find the kernel version(s) installed in the image (not the host kernel)
 KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core | tail -n1)"
 # Build kmods
